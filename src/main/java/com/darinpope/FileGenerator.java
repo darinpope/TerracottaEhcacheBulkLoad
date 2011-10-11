780aefd799ee9ce8393a6736f0632674d83f3b8e
Darin Pope <darin@planetpope.com>
2011-10-10 22:06:33 -0400

    add gradle tasks to process multiple files

--- src/main/java/com/darinpope/FileGenerator.java
+++ src/main/java/com/darinpope/FileGenerator.java
@@ -9,32 +9,39 @@ import java.util.UUID;
 public class FileGenerator {
     private static String filename;
     private static int rowCount;
+    private static int numberOfFiles;
 
     public static void main(String[] args) throws Exception {
         filename = System.getProperty("filename");
         rowCount = Integer.valueOf(System.getProperty("rowCount")).intValue();
+        numberOfFiles = Integer.valueOf(System.getProperty("numberOfFiles")).intValue();
         new FileGenerator();
     }
 
     public FileGenerator() throws Exception {
-        CSVWriter writer = new CSVWriter(new FileWriter(filename),'\t');
-        try {
-            for(int i=0;i<rowCount;i++) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(String.valueOf(i));
-                sb.append("###");
-                sb.append(UUID.randomUUID().toString());
-                sb.append("###");
-                sb.append(UUID.randomUUID().toString());
-                sb.append("###");
-                sb.append(i%100 == 0 ? "true" : "false");
-                writer.writeNext(StringUtils.split(sb.toString(),"###"));
-                if(i%10000 == 0) {
-                    System.out.println(i + " lines written");
+        int totalElements = 1;
+        for(int j=0;j<numberOfFiles;j++) {
+            String newFileName = j + "-" + filename;
+            CSVWriter writer = new CSVWriter(new FileWriter(newFileName),'\t');
+            try {
+                for(int i=0;i<rowCount;i++) {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(String.valueOf(totalElements));
+                    sb.append("###");
+                    sb.append(UUID.randomUUID().toString());
+                    sb.append("###");
+                    sb.append(UUID.randomUUID().toString());
+                    sb.append("###");
+                    sb.append(i%100 == 0 ? "true" : "false");
+                    writer.writeNext(StringUtils.split(sb.toString(),"###"));
+                    totalElements++;
+                    if(i%10000 == 0) {
+                        System.out.println(totalElements + " lines written");
+                    }
                 }
+            } finally {
+                writer.close();
             }
-        } finally {
-            writer.close();
         }
     }
 }
